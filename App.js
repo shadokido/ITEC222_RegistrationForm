472f3ba5447919a101076b9758779ab86e77d815
shadokido <119161402+shadokido@users.noreply.github.com>
2023-03-02 23:17:16 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/views/screens/HomeScreen';
+import RegistrationScreen from './src/views/screens/RegistrationScreen';
+import LoginScreen from './src/views/screens/LoginScreen';
+
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LoginScreen">
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="RegistrationScreen" component={RegistrationScreen} />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
